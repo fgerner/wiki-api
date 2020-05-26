33001a54e8e98565212c9f8769d239c5b73f7761
Fred <fred.gerner@icloud.com>
2020-05-26 22:12:04 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded(
+  { extended: true }
+));
+
+mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
+
+const articleSchema = {
+    title: String,
+    content: String
+};
+const Article = mongoose.model("Article", articleSchema);
