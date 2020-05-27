7ed9b1bd0d2437c4b1b295bc8f6cd4bb52d7e6b6
Fred <fred.gerner@icloud.com>
2020-05-27 22:31:38 +1000

    added request chain

--- app.js
+++ app.js
@@ -3,11 +3,14 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 
+const port = process.env.PORT || 3000;
+
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded(
-  { extended: true }
+    {extended: true}
 ));
+app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost:27017/wikiDB', {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -16,3 +19,40 @@ const articleSchema = {
     content: String
 };
 const Article = mongoose.model("Article", articleSchema);
+
+app.route('/articles')
+    .get(function (req, res) {
+        Article.find(function (err, foundArticles) {
+            if (!err) {
+                res.send(foundArticles)
+            } else {
+                res.send(err)
+            }
+        });
+    })
+    .post(function (req, res) {
+        const newArticle = new Article({
+            title: req.body.title,
+            content: req.body.content
+        })
+        newArticle.save(function (err) {
+            if (!err) {
+                res.send("Successfully stored")
+            } else {
+                res.send(err)
+            }
+        })
+    })
+    .delete(function (req, res) {
+        Article.deleteMany(function (err) {
+            if (!err) {
+                res.send('deleted all shit!!')
+            } else {
+                res.send(err)
+            }
+        })
+    });
+
+app.listen(port, function () {
+    console.log(`server listening on ${port}`);
+})
